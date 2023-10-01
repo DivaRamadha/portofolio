b13600a62741dd5183d15e0bd41f8aa0fec9990a
DivaRamada <diva.risanto@beps.co.id>
2023-10-01 07:34:52 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// components
+import Header from './components/Header.js';
+
+const App = () => {
+  return (
+    <div className='bg bg-no-repeat bg-cover overflow-hidden'>
+      <Header />
+      <div className='h-[4000px]'></div>
+    </div>
+  );
+};
+
+export default App;
